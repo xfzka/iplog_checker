4949de1a707f8a26a7408ee56bbdae42fcd8fd6c
baixl <baixl>
2026-01-30 16:04:58 +0800

    新增配置解析

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.25.6
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+func main() {
+	data, err := os.ReadFile("config.yaml")
+	if err != nil {
+		fmt.Printf("Error reading config file: %v\n", err)
+		return
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		fmt.Printf("Error parsing YAML: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Configuration loaded successfully:\n%+v\n", config)
+}
